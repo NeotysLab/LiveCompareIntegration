e828e859253c845804f6b1ba2ba3dad8e4ebe398
hrexed <henrik.rexed@neotys.com>
2020-05-28 11:15:18 +0200

    initial commit

--- DatabaseConnector/src/main/java/com/neotys/tricentis/MongoDB/config/Constants.java
+++ DatabaseConnector/src/main/java/com/neotys/tricentis/MongoDB/config/Constants.java
@@ -1,10 +1,10 @@
 package com.neotys.tricentis.MongoDB.config;
 
 public class Constants {
-    public static final String SECRET_HOST="HOST";
-    public static final String SECRET_PORT="PORT";
+    public static final String SECRET_HOST="MONGOHOST";
+    public static final String SECRET_PORT="MONGOPORT";
     public static final String SECRET_DATABASENAME="DATABASENAME";
-    public static final String SECRET_USER="USER";
-    public static final String SECRET_PASSWORD="PASSWORD";
+    public static final String SECRET_USER="MONGOUSER";
+    public static final String SECRET_PASSWORD="MONGOPASSWORD";
 
 }
